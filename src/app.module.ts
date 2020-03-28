19af502813ee761346b47ca88353e81ae3cb6f16
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-03-28 17:34:51 +0100

    GraphQL setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true
+    })
+  ],
 })
 export class AppModule {}
