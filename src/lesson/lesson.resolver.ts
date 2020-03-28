9e46136da72b8cf980d2a8118a4ac77927a42d14
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-03-28 19:45:47 +0100

    Implement createLesson mutation, calling service

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,8 +1,13 @@
-import { Resolver, Query, Mutation } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
 
 @Resolver(of => LessonType)
 export class LessonResolver {
+  constructor(
+    private lessonService: LessonService
+  ) {}
+
   @Query(returns => LessonType)
   lesson() {
     return {
@@ -14,7 +19,11 @@ export class LessonResolver {
   }
 
   @Mutation(returns => LessonType)
-  createLesson() {
-    
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
