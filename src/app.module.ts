89918aa199da0601ff36599a06a649696a89ae19
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-03-28 19:25:06 +0100

    Persistence + Lesson entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
+import { Lesson } from './lesson/lesson.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [
+        Lesson
+      ]
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true
     }),
---
+++ src/lesson/lesson.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryColumn, Column, ObjectIdColumn } from 'typeorm';
+
+@Entity()
+export class Lesson {
+  @ObjectIdColumn()
+  _id: string;
+
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  startDate: string;
+
+  @Column()
+  endDate: string;
+}
